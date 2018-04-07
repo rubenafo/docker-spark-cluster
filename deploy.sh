a8b98f7797f7e1df95a1427b441c11d5e1c69931
figurebelow <ruben@figurebelow.com>
2018-04-07 10:09:16 +0100

    Add cluster hosts to /etc/hosts

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ docker network rm sparknet
 docker network create --subnet=172.18.0.0/16 sparknet # create custom network
 
 # 3 nodes
-docker run -d --net sparknet --ip 172.18.0.2 --hostname node1 -it scalabase
-docker run -d --net sparknet --ip 172.18.0.3 --hostname node2 -it scalabase
-docker run -d --net sparknet --ip 172.18.0.4 --hostname node3 -it scalabase
+docker run -d --net sparknet --ip 172.18.1.1 --hostname node1 --add-host node2:172.18.1.2 --add-host node3:172.18.1.3 -it scalabase
+docker run -d --net sparknet --ip 172.18.1.2 --hostname node2 --add-host node1:172.18.1.1 --add-host node3:172.18.1.3 -it scalabase
+docker run -d --net sparknet --ip 172.18.1.3 --hostname node3 --add-host node1:172.18.1.1 --add-host node2:172.18.1.2 -it scalabase
