f9d82765a374e5f557531ec6bbc8de14ff94c1e1
figurebelow <ruben@figurebelow.com>
2018-04-08 09:36:13 +0100

    Reorganize build

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ docker network rm sparknet
 docker network create --subnet=172.18.0.0/16 sparknet # create custom network
 
 # 3 nodes
-docker run -d --net sparknet --ip 172.18.1.1 --hostname node1 --add-host node2:172.18.1.2 --add-host node3:172.18.1.3 -it scalabase
-docker run -d --net sparknet --ip 172.18.1.2 --hostname node2 --add-host node1:172.18.1.1 --add-host node3:172.18.1.3 -it scalabase
-docker run -d --net sparknet --ip 172.18.1.3 --hostname node3 --add-host node1:172.18.1.1 --add-host node2:172.18.1.2 -it scalabase
+docker run -d --net sparknet --ip 172.18.1.1 --hostname nodemaster --add-host node2:172.18.1.2 ---add-host node3:172.18.1.3 -it hadoopbase
+docker run -d --net sparknet --ip 172.18.1.2 --hostname node2  --add-host nodemaster:172.18.1.1 --add-host node3:172.18.1.3 -it hadoopbase
+docker run -d --net sparknet --ip 172.18.1.3 --hostname node3  --add-host nodemaster:172.18.1.1 --add-host node2:172.18.1.2 -it hadoopbase
