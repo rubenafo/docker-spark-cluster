c8031e683f0a246c17cddd01c6edf3cd67da3f3d
figurebelow <ruben@figurebelow.com>
2018-04-17 06:36:29 +0100

    Bild script downloads Hadoop .gz

--- hadoop/build.sh
+++ hadoop/build.sh
@@ -1,4 +1,6 @@
 
 ssh-keygen -t rsa -P '' -f config/id_rsa
 
+mkdir -p deps
+wget http://mirrors.whoishostingthis.com/apache/hadoop/common/hadoop-3.1.0/hadoop-3.1.0.tar.gz -P ./deps
 docker build . -t hadoopbase
