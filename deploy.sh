a707f1fb699c23db0d240666950dd1816f76f17b
figurebelow <ruben@figurebelow.com>
2018-04-24 07:10:45 +0100

    Add Spark setup

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,12 @@ docker network rm sparknet
 docker network create --subnet=172.18.0.0/16 sparknet # create custom network
 
 # 3 nodes
+echo ">> Starting nodes master and worker nodes ..."
 docker run -d --net sparknet --ip 172.18.1.1 --hostname nodemaster --add-host node2:172.18.1.2 --add-host node3:172.18.1.3 --name nodemaster -it hadoopbase
 docker run -d --net sparknet --ip 172.18.1.2 --hostname node2  --add-host nodemaster:172.18.1.1 --add-host node3:172.18.1.3 --name node2 -it hadoopbase
 docker run -d --net sparknet --ip 172.18.1.3 --hostname node3  --add-host nodemaster:172.18.1.1 --add-host node2:172.18.1.2 --name node3 -it hadoopbase
+
+# Format nodemaster
+echo ">> Formatting hdfs ..."
+docker exec -u hadoop -it nodemaster hadoop/bin/hdfs namenode -format
+docker exec -u hadoop -it nodemaster hadoop/sbin/start-dfs.sh
--- hadoop/build.sh
+++ hadoop/build.sh
@@ -3,4 +3,6 @@ ssh-keygen -t rsa -P '' -f config/id_rsa
 
 mkdir -p deps
 wget http://mirrors.whoishostingthis.com/apache/hadoop/common/hadoop-3.1.0/hadoop-3.1.0.tar.gz -P ./deps
+wget http://mirrors.whoishostingthis.com/apache/spark/spark-2.3.0/spark-2.3.0-bin-without-hadoop.tgz -P ./deps
+
 docker build . -t hadoopbase
