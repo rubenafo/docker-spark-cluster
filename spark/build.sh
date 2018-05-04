53cab68a06529abec4d1f3ea7a91e18f8629e06e
figurebelow <ruben@figurebelow.com>
2018-05-04 06:59:01 +0100

    Skip hadoop and spark .tgz downloads if they exist

--- spark/build.sh
+++ spark/build.sh
@@ -1,10 +1,13 @@
 
 echo "Y" | ssh-keygen -t rsa -P '' -f config/id_rsa
 
-mkdir -p deps
-
-# Uncomment to retrieve the dependencies
-#wget http://mirrors.whoishostingthis.com/apache/hadoop/common/hadoop-3.1.0/hadoop-3.1.0.tar.gz -P ./deps
-#wget http://mirrors.whoishostingthis.com/apache/spark/spark-2.3.0/spark-2.3.0-bin-without-hadoop.tgz -P ./deps
+if [ ! -d "deps" ]; then
+  mkdir -p deps
+  echo "Downloading hadoop, spark dependencies"
+  wget http://mirrors.whoishostingthis.com/apache/hadoop/common/hadoop-3.1.0/hadoop-3.1.0.tar.gz -P ./deps
+  wget http://mirrors.whoishostingthis.com/apache/spark/spark-2.3.0/spark-2.3.0-bin-without-hadoop.tgz -P ./deps
+else
+  echo "Dependencies found, skipping retrieval..."
+fi
 
 docker build . -t sparkbase
