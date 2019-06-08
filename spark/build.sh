57851fedb15d343a76eac04c32d2c9b0c0b765a9
figurebelow <ruben@figurebelow.com>
2019-06-08 09:21:17 +0100

    Fix hadoop/spark paths. Fix invalid docker JAVA_HOME

--- spark/build.sh
+++ spark/build.sh
@@ -1,13 +1,4 @@
 
 echo "Y" | ssh-keygen -t rsa -P '' -f config/id_rsa
 
-if [ ! -d "deps" ]; then
-  mkdir -p deps
-  echo "Downloading hadoop, spark dependencies"
-  wget https://archive.apache.org/dist/hadoop/core/hadoop-3.2.0/hadoop-3.2.0.tar.gz -P ./deps
-  wget https://archive.apache.org/dist/spark/spark-2.4.0/spark-2.4.0-bin-without-hadoop.tgz -P ./deps
-else
-  echo "Dependencies found, skipping retrieval..."
-fi
-
-docker build . -t sparkbase
+docker build . -t sparkbase #--no-cache=true
